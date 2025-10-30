160b83a07a67d21b248725a8f911ad4683af70bb
Manuel Prince <manuelsdesk0029@gmail.com>
2025-10-30 10:00:54 +0100

    Day 37  i made more features

--- main.py
+++ main.py
@@ -1,47 +1,62 @@
 import requests
+from datetime import datetime
 
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+pixela_endpoint = 'https://pixe.la/v1/users'
+USERNAME = "manuels"
+TOKEN = "#############"
 
-api_key = "be2b744511fb2d76bec521e2531e2578"
-latitude = 5.181740
-longitude = 7.715100
-
-#Making a request
-parameters = {
-    "lat": latitude,
-    "lon": longitude,
-    "appid": api_key,
-    "cnt": 4,
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
 }
-response = requests.get("https://api.openweathermap.org/data/"
-             "2.5/forecast", params=parameters)
 
-will_rain = False
-response.raise_for_status()
-weather_data = response.json()
-for item in weather_data['list']:
-    if int(item['weather'][0]['id']) < 700:
-        will_rain = True
-if will_rain:
-    print("Bring an Umbrella")
-    #User info
-    user_name = 'my email'
-    user_password = 'my password'
-    #Create Message Body
-    subject = f'Raining Alert'
-    body = "Hey it'll rain today before 6pm"
 
-    message = MIMEMultipart()
-    message['To'] = user_name
-    message['From'] = user_name
-    message['Subject'] = subject
+#response = requests.post(url=pixela_endpoint, json=user_params)
+#print(response.text)
 
-    message.attach(MIMEText(body, "plain"))
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login(user_name, user_password)
-        server.send_message(message)
-    print("Email sent successfully")
+graph_config = {
+    'id': 'graph1',
+    "name": 'Cycling Graph',
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+#response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+#print(response.text)
+
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}"
+
+today = datetime(year=2025, month=8, day=31)
+
+pixel_params = {
+    'date': today.strftime("%Y%m%d"),
+    'quantity': '15'
+}
+
+#response = requests.post(url=pixel_endpoint, json=pixel_params, headers=headers)
+#print(response.text)
+
+
+updated_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}/{pixel_params['date']}"
+
+updated_params = {
+    'quantity': '4'
+}
+
+#response = requests.put(url=updated_endpoint, json=updated_params, headers=headers)
+#print(response.text)
+
+
+deleted_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}/{pixel_params['date']}"
+
+response = requests.delete(url=deleted_endpoint, headers=headers)
+print(response.text)
