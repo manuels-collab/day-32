89d3d5d2f8e048e988ce8b0e5ab95618ed21964d
Manuel Prince <manuelsdesk0029@gmail.com>
2025-10-30 09:57:30 +0100

    Called and API and sent an Email with Py bots

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
-from question_model import Question
-from data import question_data
-from quiz_brain import QuizBrain
-from ui import QuizInterface
+import requests
 
-question_bank = []
-for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
+api_key = "be2b744511fb2d76bec521e2531e2578"
+latitude = 5.181740
+longitude = 7.715100
 
-quiz = QuizBrain(question_bank)
-quiz_ui = QuizInterface(quiz)
+#Making a request
+parameters = {
+    "lat": latitude,
+    "lon": longitude,
+    "appid": api_key,
+    "cnt": 4,
+}
+response = requests.get("https://api.openweathermap.org/data/"
+             "2.5/forecast", params=parameters)
 
-#while quiz.still_has_questions():
- #   quiz.next_question()
+will_rain = False
+response.raise_for_status()
+weather_data = response.json()
+for item in weather_data['list']:
+    if int(item['weather'][0]['id']) < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an Umbrella")
+    #User info
+    user_name = 'my email'
+    user_password = 'my password'
+    #Create Message Body
+    subject = f'Raining Alert'
+    body = "Hey it'll rain today before 6pm"
 
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+    message = MIMEMultipart()
+    message['To'] = user_name
+    message['From'] = user_name
+    message['Subject'] = subject
+
+    message.attach(MIMEText(body, "plain"))
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(user_name, user_password)
+        server.send_message(message)
+    print("Email sent successfully")
