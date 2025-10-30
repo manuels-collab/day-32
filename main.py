8440d27c29e4d6668d2c12bfd2b2bb2c751adb6d
Manuel Prince <manuelsdesk0029@gmail.com>
2025-10-30 10:02:39 +0100

    I worked with AIRFLIGHT APIS

--- main.py
+++ main.py
@@ -1,62 +1,62 @@
-import requests
-from datetime import datetime
+import time
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
+from notification_manager import NotificationManager
 
-pixela_endpoint = 'https://pixe.la/v1/users'
-USERNAME = "manuels"
-TOKEN = "#############"
+# ==================== Set up the Flight Search ====================
 
-user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes"
-}
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+# Set your origin airport
+ORIGIN_CITY_IATA = "LON"
+
+# ==================== Update the Airport Codes in Google Sheet ====================
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+        # slowing down requests to avoid rate limit
+        time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
+
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
+
+# ==================== Search for Flights and Send Notifications ====================
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination['city']}...")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slowing down requests to avoid rate limit
+    time.sleep(2)
+
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        print(f"Lower price flight found to {destination['city']}!")
+        # notification_manager.send_sms(
+        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        # )
+        # SMS not working? Try whatsapp instead.
+        notification_manager.send_whatsapp(
+            message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        )
 
 
-#response = requests.post(url=pixela_endpoint, json=user_params)
-#print(response.text)
-
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-
-graph_config = {
-    'id': 'graph1',
-    "name": 'Cycling Graph',
-    "unit": "Km",
-    "type": "float",
-    "color": "ajisai"
-}
-
-headers = {
-    "X-USER-TOKEN": TOKEN
-}
-
-#response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-#print(response.text)
-
-pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}"
-
-today = datetime(year=2025, month=8, day=31)
-
-pixel_params = {
-    'date': today.strftime("%Y%m%d"),
-    'quantity': '15'
-}
-
-#response = requests.post(url=pixel_endpoint, json=pixel_params, headers=headers)
-#print(response.text)
-
-
-updated_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}/{pixel_params['date']}"
-
-updated_params = {
-    'quantity': '4'
-}
-
-#response = requests.put(url=updated_endpoint, json=updated_params, headers=headers)
-#print(response.text)
-
-
-deleted_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}/{pixel_params['date']}"
-
-response = requests.delete(url=deleted_endpoint, headers=headers)
-print(response.text)
