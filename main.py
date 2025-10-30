03370a65c1c0db9a88d9cacc80f254b040374c56
Manuel Prince <manuelsdesk0029@gmail.com>
2025-10-30 09:55:40 +0100

    On day 34 i increased UI and UX designs'

--- main.py
+++ main.py
@@ -1,65 +1,21 @@
-import requests
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from datetime import datetime
-import time
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from ui import QuizInterface
 
-MY_LAT =  5.172110# Your latitude
-MY_LONG = 7.999480 # Your longitude
-
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-iss_data = response.json()
-
-iss_latitude = 7.172110 #float(data["iss_position"]["latitude"])
-iss_longitude = 3.4444#float(data["iss_position"]["longitude"])
-
-#Your position is within +5 or -5 degrees of the ISS position.
-def set_current_position(lat, long):
-    global iss_latitude, iss_longitude
-    if lat-5 <= iss_latitude <= lat + 5 or long-5 <= iss_longitude <= long+5:
-            print(iss_latitude)
-            print(iss_longitude)
-
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
-
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = 17#int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = 7#int(data["results"]["sunset"].split("T")[1].split(":")[0])
-time_now = datetime.now()
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 
-#If the ISS is close to my current position
-# and it is currently dark
-if time_now.hour >= sunset or time_now.hour <= sunrise:
-    set_current_position(MY_LAT, MY_LONG)
-# Then email me to tell me to look up.
-    #Create an email
-    while True:
-        my_email = "My email"
-        password = "My Password"
-        subject = "ISS in the sky"
-        body = "Look up in the sky, the ISS IS PASSING"
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
 
-        message = MIMEMultipart()
-        message['From'] = my_email
-        message['To'] = my_email
-        message['Subject'] = subject
+#while quiz.still_has_questions():
+ #   quiz.next_question()
 
-        message.attach(MIMEText(body, 'plain'))
-        with smtplib.SMTP("smtp.gmail.com", 587) as server:
-            server.starttls()
-            server.login(my_email, password)
-            server.send_message(message)
-        print("Messages sent successfully")
-# BONUS: run the code every 60 seconds.
-
-
-
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
